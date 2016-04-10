b1ea8d2da6ea5af0a151e783401fb670a7ca4c9d
Arep0 <NrepO@outlook.com>
2016-04-10 13:25:35 -0500

    added themes

--- packages.sh
+++ packages.sh
@@ -3,3 +3,5 @@ dpkg-deb -bZgzip projects/AList debs
 dpkg-deb -bZgzip projects/AiconPkg debs
 dpkg-deb -bZgzip projects/Liquefied debs
 dpkg-deb -bZgzip projects/NsB debs
+dpkg-deb -bZgzip projects/NsDark debs
+dpkg-deb -bZgzip projects/NsLight debs
